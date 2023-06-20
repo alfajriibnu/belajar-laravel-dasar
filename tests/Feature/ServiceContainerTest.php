e6138010416116fb427ab9091a697719b60e6ef7
alfajriibnu <alfajriibnu1553@gmail.com>
2023-06-21 00:21:02 +0800

    instance

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -55,6 +55,22 @@ class ServiceContainerTest extends TestCase
         self::assertSame($person1, $person2);
    }
 
+   public function testInstance()
+   {
+        $person = new Person("Eko", "Khannedy");
+        $this->app->instance(Person::class, $person);
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals('Eko', $person1->firstName);
+        self::assertEquals('Eko', $person2->firstName);
+        self::assertSame($person, $person1);
+        self::assertSame($person1, $person2);
+
+    
+   }
+
 
 }
 
