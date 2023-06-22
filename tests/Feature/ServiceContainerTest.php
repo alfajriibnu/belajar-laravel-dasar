2aa9ab22e1bd8b4e84eba4b1f644b79a6057acfa
alfajriibnu <alfajriibnu1553@gmail.com>
2023-06-22 17:27:42 +0800

    ServiceContainerTest InterfaceToClass

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -4,9 +4,8 @@ namespace Tests\Feature;
 use App\Data\Bar;
 use App\Data\Foo;
 use App\Data\Person;
-
-
-
+use App\Services\HelloService;
+use App\Services\HelloServiceIndonesia;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,8 +14,8 @@ class ServiceContainerTest extends TestCase
 {
    public function testDependency()
    {
-        $foo1 = $this->app->make(Foo::class);
-        $foo2 = $this->app->make(Foo::class);
+        $foo1 = $this->app->make(Foo::class); // new Foo();
+        $foo2 = $this->app->make(Foo::class); // new Foo();
 
         self::assertEquals('Foo', $foo1->foo());
         self::assertEquals('Foo', $foo2->foo());
@@ -30,7 +29,7 @@ class ServiceContainerTest extends TestCase
         return new Person("Ibnu", "Alfajri");
      });
 
-     $person1 = $this->app->make(Person::class);
+     $person1 = $this->app->make(Person::class); //closure() // new person ()
      $person2 = $this->app->make(Person::class);
 
      self::assertEquals('Ibnu', $person1->firstName);
@@ -74,7 +73,7 @@ class ServiceContainerTest extends TestCase
       $this->app->singleton(Foo::class, function($app){
          return new Foo();
       });
-      
+
       $foo = $this->app->make(Foo::class);
       $bar1 = $this->app->make(Bar::class);
       $bar2 = $this->app->make(Bar::class);
@@ -83,4 +82,19 @@ class ServiceContainerTest extends TestCase
 
       self::assertNotSame($bar1, $bar2);
    }
+
+   public function testInterfaceToClass()
+   {
+    // $this->app->singleton(HelloService::class HelloServiceIndonesia::class);
+
+    $this->app->singleton(HelloService::class, function($app){
+        return new HelloServiceIndonesia();
+    });
+
+    $helloService = $this->app->make(HelloService::class);
+
+    self::assertEquals('Halo Ibnu', $helloService->hello('Ibnu'));
+   }
+
+
 }
